b4cc8e7a299c3dd2e425aec7d2f8a4998a835cfc
liujunning <liujunning@meituan.com>
2022-07-13 20:34:17 +0800

    jest练习

--- src/reactivity/test/index.spec.ts
+++ src/reactivity/test/index.spec.ts
@@ -4,3 +4,13 @@ it('init', ()=> {
     expect(add(2,3)).toBe(5)
     expect(add(1,1)).toBe(2)
 })
+
+test('joke', ()=> {
+    expect(add(3,4)).toEqual(7)
+})
+
+test('object val', ()=> {
+    let data = {one: 1}
+    data['two'] = 2;
+    expect(data).toEqual({one: 1, two: 2})
+})
